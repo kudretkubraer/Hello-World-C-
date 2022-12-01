c38b654559b9c9c1e42d0a1077e19dbb63d050df
Kübra <119053538+kudretkubraer@users.noreply.github.com>
2022-12-01 15:05:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace proje1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
